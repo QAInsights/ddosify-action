acee5069079d6836c0bf0ab5f20274a667621415
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2021-11-14 15:44:57 -0500

    Updated README

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@ go version # Check Go version
 
 go install -v go.ddosify.com/ddosify@latest # Install ddosify
 
-ddosify $@ > $(date +%s).log # Run ddosify
+ddosify $@ > $(date +%s).log # Run ddosify and store the results to *.log in current directory
